c771fc2691c6d14510702eb721edfebef41cc3f4
ziad-abdul-samad <128688549+ziad-abdul-samad@users.noreply.github.com>
2023-10-06 13:46:27 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,3 +1,17 @@
+const sr = ScrollReveal ({
+    distance:'90px',
+    duration:"1500",
+    delay:"100",
+    reset:true
+    }) ;
+    
+sr.reveal('.header' ,{delay:200 , origin: "left"} );
+sr.reveal('.filter' ,{delay:500 , origin: "right"} );
+sr.reveal('.landing h1' ,{delay:300 , origin: "top"} );
+sr.reveal('.landing p' ,{delay:400 , origin: "top"} );
+sr.reveal('.landing img' ,{delay:600 , origin: "top"} );
+sr.reveal('.products' ,{delay:200 , origin: "top"} );
+
 let switcher = document.querySelectorAll(".filter li");
 let products = document.querySelectorAll(".products .product");
 switcher.forEach((li) => {
@@ -11,7 +25,7 @@ function removeactive() {
     });
 }
 function manage() {
- products.forEach((product) => {
+products.forEach((product) => {
     product.style.display = "none"
 document.querySelectorAll(this.dataset.cat).forEach((el) => {
     el.style.display = "block";
